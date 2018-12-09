3ff9aee94dfe4328a306059c245e013f7cdeaf8b
ZHANG_JIAN_FEI <zhangjianfeiso@163.com>
2018-12-09 23:31:09 +0800

    @@@@

---
+++ housing3/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+    #app {
+      font-family: 'Avenir', Helvetica, Arial, sans-serif;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+      text-align: center;
+      color: #2c3e50;
+    }
+    #flow-list a .yd-tabbar-txt{
+        margin-right: 15px;
+        padding: 2px;
+        color:#999;
+    }
+
+   /* .yd-list-img{
+        height: 130px!important;
+        padding: 0 0!important;
+    }*/
+</style>
---
+++ housing3/src/main.js
@@ -0,0 +1,37 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import YDUI from 'vue-ydui'
+/* 相当于import YDUI from 'vue-ydui/ydui.rem.js' */
+import 'vue-ydui/dist/ydui.rem.css'
+import {NavBar, NavBarBackIcon, NavBarNextIcon} from 'vue-ydui/dist/lib.rem/navbar'
+import {TabBar, TabBarItem} from 'vue-ydui/dist/lib.rem/tabbar'
+import {Layout} from 'vue-ydui/dist/lib.rem/layout'
+import {CitySelect} from 'vue-ydui/dist/lib.rem/cityselect'
+import {Popup} from 'vue-ydui/dist/lib.rem/popup'
+import VueJsonp from 'vue-jsonp'
+import VueResource from 'vue-resource'
+
+Vue.use(VueResource)
+Vue.use(VueJsonp)
+Vue.use(YDUI)
+Vue.component(NavBar.name, NavBar)
+Vue.component(NavBarBackIcon.name, NavBarBackIcon)
+Vue.component(NavBarNextIcon.name, NavBarNextIcon)
+Vue.component(TabBar.name, TabBar)
+Vue.component(TabBarItem.name, TabBarItem)
+Vue.component(Layout.name, Layout)
+Vue.component(Popup.name, Popup)
+Vue.component(CitySelect.name, CitySelect)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+    el: '#app',
+    router,
+    components: {App},
+    template: '<App/>'
+})
